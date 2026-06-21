8c73fa0f119add46a528a330daba653814f6a5c0
Nanayaw200 <opokuyaweffah@gmail.com>
2026-06-21 11:14:05 +0000

    Use Andamologo.png as site favicon

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import logo from './assets/Andamologo.png'
+
+const favicon = document.querySelector<HTMLLinkElement>("link[rel~='icon']")
+if (favicon) {
+  favicon.href = logo
+} else {
+  const link = document.createElement('link')
+  link.rel = 'icon'
+  link.href = logo
+  document.head.appendChild(link)
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
